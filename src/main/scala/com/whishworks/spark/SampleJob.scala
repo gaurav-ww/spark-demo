acf3ba707800530db020ad102875f6950bba852f
gaurav-ww <gaurav.bhardwaj@whishworks.com>
2018-08-01 14:56:12 +0100

    Adding hive table functionality. Hive table is called demo

--- src/main/scala/com/whishworks/spark/SampleJob.scala
+++ src/main/scala/com/whishworks/spark/SampleJob.scala
@@ -1,15 +1,31 @@
 package com.whishworks.spark
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 
 object SampleJob {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Sample App")
-    val sc = new SparkContext(conf)
-    val dummyData = 10 to 0 by -1
+    val session = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
+    val sc = session.sparkContext
+    //val sc = new SparkContext(conf)
+    val format = new java.text.SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+    val currentTime = format.format(new java.util.Date())
+    val dummyData = (1 to 10).map(Row(_, currentTime))
+
     val rdd = sc.parallelize(dummyData, 1)
-    val data = rdd.collect()
-    data.foreach(println)
+
+
+    session.sql("CREATE TABLE IF NOT EXISTS demo(id INT, cur_time STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+
+    val schema = StructType(Seq(
+      StructField("product", IntegerType, false),
+      StructField("aldi_price", StringType, false)))
+
+    val df = session.createDataFrame(rdd, schema)
+    df.write.mode(SaveMode.Overwrite).format("hive").saveAsTable("demo")
+
     println("Everything is working fine!")
     sc.stop()
   }
